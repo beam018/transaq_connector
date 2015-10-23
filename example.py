104b4d2a9c59a60aaf24392a8435aa53a8ea9a5d
Egor Svetlakov ( beam ) <beam018@gmail.com>
2015-10-23 15:18:36 +0200

    add logs dir and fix example

--- example.py
+++ example.py
@@ -12,8 +12,8 @@ def handle_txml_message(msg):
 
 if __name__ == '__main__':
     try:
-        initialize("Logs", 3, handle_txml_message)
-        print connect("ЛОГИН", "ПАРОЛЬ", "78.41.194.46:3950")
+        initialize("logs", 3, handle_txml_message)
+        print connect("test-login", "test-password", "213.247.141.133:3900")
         time.sleep(3)
         print get_history('TQBR', 'GAZP', 2, count=10)
         time.sleep(3)
